e61dd431fb9c86fc5919e3602e28147f57908aa8
Michael Calkins <m.calkins576@gmail.com>
2019-10-25 14:05:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,7 +2,25 @@
 require 'pry'
 
 def game_hash
-the_game = {:home => {:team_name => {}, :colors => {}, :players => {}}, :away => {}}
+the_game = {:home => {
+      :team_name => {},
+      :colors => {}, 
+      :players => {
+        [{:player_name => ""},
+         {:number => ""},
+         {:shoe => ""},
+         {:points => ""},
+         {:rebounds => ""},
+         {:assists => ""},
+         {:steals => ""},
+         {:blocks => ""},
+         {:slam_dunks =>""}]}}, 
+    {:away => {
+      :team_name => {}, 
+      :colors => {}, 
+      :players => {
+        
+      }}}
 
 
 
