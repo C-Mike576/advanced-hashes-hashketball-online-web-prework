bce2b90e7c2c233574272a4849e36407d35d419f
Michael Calkins <m.calkins576@gmail.com>
2019-10-25 15:07:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -109,7 +109,7 @@ def num_points_scored(name)
       player.each do |key, value|
         #binding.pry
         if value == name
-          return player[:points].to_
+          return player[:points].to_i
         end
       end
     end
